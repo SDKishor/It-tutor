93df1aef130b4737e4141a523f5fcb5547e38dce
SDKishor <91415355+SDKishor@users.noreply.github.com>
2022-05-17 20:35:50 +0600

    header scetion done

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./home.css";
+
+export const Home = () => {
+  return (
+    <div className="homePage">
+      <div className="banner">
+        <h2>Information is the Oxygen of The Modern Age</h2>
+        <div className="learnMore">
+          <p>So lets </p>
+          <button>Learn More</button>
+        </div>
+      </div>
+    </div>
+  );
+};
