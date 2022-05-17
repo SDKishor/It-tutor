9abbee75e2191706db45e5f1edd94d4303744c37
SDKishor <91415355+SDKishor@users.noreply.github.com>
2022-05-17 22:15:05 +0600

    service scetion done

---
+++ src/components/service/Service.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./service.css";
+
+export const Service = () => {
+  return (
+    <div className="service">
+      <div className="Serviceimage">
+        <img
+          src="https://images.unsplash.com/photo-1565106430482-8f6e74349ca1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+          alt=""
+        />
+      </div>
+      <h2>Title</h2>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iste
+        voluptatibus, est distinctio earum quo consectetur.
+      </p>
+      <button>Learn More</button>
+    </div>
+  );
+};
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Footer } from "../../components/footer/Footer";
+import { Service } from "../../components/service/Service";
 import "./home.css";
 
 export const Home = () => {
@@ -7,12 +8,16 @@ export const Home = () => {
     <div className="homePage">
       <div className="banner">
         <h2>Information is the Oxygen of The Modern Age</h2>
-        <div className="learnMore">
-          <p>So lets </p>
-          <button>Learn More</button>
-        </div>
       </div>
-
+      <div className="serviceSec">
+        <h2>Popular Services</h2>
+        <div className="services">
+          <Service></Service>
+          <Service></Service>
+          <Service></Service>
+        </div>
+        <button className="seeMoreBtn">SEE MORE</button>
+      </div>
       <Footer></Footer>
     </div>
   );
